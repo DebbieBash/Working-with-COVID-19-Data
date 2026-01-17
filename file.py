654d6fa72caef063148749ed9346f73158316576
DebbieBash <139637522+DebbieBash@users.noreply.github.com>
2026-01-17 06:06:56 +0000

    Imported the data set

---
+++ file.py
@@ -0,0 +1,17 @@
+# Using the provided COVID-19 sample data, attempt the following tasks:
+
+#  1. Find the country with the highest number of active cases
+
+#  2. Calculate the ratio of active cases to population for each country
+
+# 3. Find the country with the highest recovery rate (recoveries/total_cases)
+
+# 4. Calculate active cases as a percentage of population for each country
+
+# 5. Generate a new dictionary showing which countries have more than 20,000 active cases
+
+# 6. Calculate the total number of active cases across all countries
+
+# 7. Add a new key called "cases_per_million" to each country based on total_cases
+
+# 8. Using user input, update the active cases for a specific country and recalculate the global total
